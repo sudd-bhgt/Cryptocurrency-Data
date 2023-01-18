494c665d74fdf7beced3e1553a83299b5af7d65f
sudd-bhgt <36042730+sudd-bhgt@users.noreply.github.com>
2023-01-18 12:58:30 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ async function promise() {
     const response = await fetch(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
     );
-     console.log(result);
+     console.log(response);
 
-    const res = await result.json();
+    const res = await response.json();
      console.log(res);
 
     for (let i = 0; i < res.length; i++) {
